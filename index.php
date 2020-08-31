475cb5e87a3815c68e4307648391964fd8743c9b
Andreas Heigl <andreas@heigl.org>
2020-08-31 21:02:02 +0200

    Add a test-file
    Call it via php index.php and get a random name

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright Andreas Heigl <andreas@heigl.org>
+ *
+ * Licenses under the MIT-license. For details see the included file LICENSE.md
+ */
+require __DIR__ . '/vendor/autoload.php';
+
+echo \Org_Heigl\CommunityNaming\Generator::generate();
